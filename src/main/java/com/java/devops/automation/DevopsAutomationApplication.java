d922c14060756b34b7bbf1e962784c631c94d554
Sunil Meher <mehersunil734@gmail.com>
2025-12-01 15:54:26 +0530

    added one methos in main class

--- src/main/java/com/java/devops/automation/DevopsAutomationApplication.java
+++ src/main/java/com/java/devops/automation/DevopsAutomationApplication.java
@@ -2,9 +2,15 @@ package com.java.devops.automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsAutomationApplication {
+	
+	public String getName() {
+		return "name getting successfully";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsAutomationApplication.class, args);
